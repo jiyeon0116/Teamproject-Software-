5f0b69ec7024a0c127972887924f6ff015cc04db
ayaka <ito.ayaka@gmail.com>
2019-11-24 17:20:16 +0900

    Book 클래스 수정

--- Book.java
+++ Book.java
@@ -1,33 +1,30 @@
-
+import java.util.*;
 /**
- * クラス Book の注釈をここに書きます.
+ *  Book 객체에 관한 클래스 
  * 
- * @author (あなたの名前)
- * @version (バージョン番号もしくは日付)
+ * @author (2017315056 이토아야카)
+ * @version (2019.11.25)
  */
 public class Book
 {
-    // インスタンス変数 - コードに合わせて説明を書き換えます.
-    private int x;
-
-    /**
-     * Book クラスのインスタンスのためのコンストラクタ
-     */
-    public Book()
-    {
-        // インスタンス変数の初期化
-        x = 0;
+    private TreeSet<Book> ts;
+    private int catalogNumber;
+    private String author;
+    private String title;
+    
+    public Book(){
+        
     }
-
-    /**
-     * メソッドの例 - メソッドとともにこのコメントも置き換えてください.
-     * 
-     * @param  y    メソッドのためのサンプル引数
-     * @return        x と y の和
-     */
-    public int sampleMethod(int y)
-    {
-        // ここにあなたのコードを書きます
-        return x + y;
+    
+    public attachBook(){
+        
+    }
+    
+    public dettachBook(){
+        
+    }
+    
+    public DisplayBook(){
+        
     }
 }
