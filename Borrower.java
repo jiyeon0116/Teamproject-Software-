5011c9f91b4b4afbcffcd6a781d67462b403baa9
jiyeon <gbko159@naver.com>
2019-11-24 16:38:09 +0900

    파라메타 및 메소드 작성

--- Borrower.java
+++ Borrower.java
@@ -7,27 +7,11 @@
  */
 public class Borrower
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 바꾸십시오
-    private int x;
-
-    /**
-     * Borrower 클래스의 객체 생성자
-     */
-    public Borrower()
-    {
-        // 인스턴스 변수의 초기화
-        x = 0;
+    private String name;
+    
+    public Loan(CheckBook,CheckUser){
+        return book
     }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     * 
-     * @param  y   메소드의 예제 매개변수
-     * @return     x 더하기 y 
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하십시오.
-        return x + y;
+    public Return(CheckBook,CheckUser){        
     }
 }
