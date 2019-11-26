a75d5b09dfb3e3e2e8a70614f9729a5fe8090a98
jiyeon <gbko159@naver.com>
2019-11-26 12:46:34 +0900

    borrower

--- Borrower.java
+++ Borrower.java
@@ -2,7 +2,7 @@ java.util.*;
 /**
  * 여기에 Borrower 클래스 설명을 작성하십시오.
  * 
- * @author (작성자 이름) 
+ * @author (2018315022 이지연) 
  * @version (버전번호나 날짜)
  */
 public class Borrower
@@ -12,10 +12,13 @@ public class Borrower
     public Borrower(){
         Hashset<Borrower> b = new Hashset<Borrower>();
     }
-    public Loan(CheckBook,CheckUser){
+    public Loan(){
+        CheckBook,CheckUser
         return book;
     }
-    public void Return(CheckBook,CheckUser){        
+    public void Return(){  
+        CheckBook,CheckUser
     }
+    
     
 }
