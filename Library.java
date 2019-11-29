42c26cf9793cd3e8991c1cd090e926469a8cbb1b
ayaka <ito.ayaka@gmail.com>
2019-11-29 13:36:53 +0900

    주석문 수정

--- Library.java
+++ Library.java
@@ -3,7 +3,7 @@ import java.util.*;
  * Library에 대한 클래스
  *
  * @author (2018315022 이지연,2017315056 이토아야카,2018315010 박채린, 2018315059 이와사키 메구미)
- * @version (2019.11.26)
+ * @version (2019.11.29)
  */
 public class Library
 {
