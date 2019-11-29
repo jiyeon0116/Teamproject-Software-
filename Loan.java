05514c2814643bed20b91d2ebb12c376013138cd
ParkChaeLin-818 <qkrcofls6863@gmail.com>
2019-11-29 14:16:49 +0900

    Loan Date방법 수정했습니다

--- Loan.java
+++ Loan.java
@@ -14,16 +14,24 @@ public class Loan
     private int Year = oCalendar.get(Calendar.YEAR);
     private int Month = oCalendar.get(Calendar.MONTH +1);
     private int Day = oCalendar.get(Calendar.DAY_OF_MONTH);
+    
     public void Loan_Date()
     {
-        System.out.println("년: " + Year);
-        System.out.println("월: " + Month);
-        System.out.println("일: " + Day);
+        if(Attach()=true)
+        {
+            System.out.println("년: " + Year);
+            System.out.println("월: " + Month);
+            System.out.println("일: " + Day);
+        }
     }
     public void Return_Date()
     {
-        System.out.println("년: " + Year);
-        System.out.println("월: " + Month);
-        System.out.println("일: " + (Day+14));
+        if(Attach()=true)
+        {
+            System.out.println("년: " + Year);
+            System.out.println("월: " + Month);
+            System.out.println("일: " + (Day+14));
+        }
+        
     }
 }
